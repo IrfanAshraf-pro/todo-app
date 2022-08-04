39c71c0cb72c2276bdfcd1d6196c76e481e04a47
IrfanAshraf-pro <irfanashraf4090@gmail.com>
2022-08-04 05:15:27 -0700

    all done with simple todo

--- src/components/Todo.jsx
+++ src/components/Todo.jsx
@@ -1,46 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BsCheck2Circle } from 'react-icons/bs'
 import {BiEdit} from 'react-icons/bi'
 import {RiDeleteBin5Fill} from 'react-icons/ri'
-function Todo({ item, handleDelete,handleEdit, handleCompleted }) {
+function Todo({ item, handleDelete, handleEdit, handleCompleted }) {
+  const [comp,setComp]=useState('white')
   const deleteItem=() => {
     handleDelete(item.id)
   }
   const edit = () => {
     handleEdit(item)
   }
+  useEffect(() => {
+    if (item.completed) {
+      setComp('green')
+    }else{
+      setComp('white')
+    }
+  },[item.completed])
   const completed = () => {
-    handleCompleted(item.id,!item.completed)
+    handleCompleted(item.id, !item.completed)
   }
+  
   return (
       <div className={`todo-container ${item.completed ?'completed' : 'unComplete'}`}>
           <div className="todo-icons">
-              <BiEdit
-                className="edit-icon icon"
+              {!item.completed && <BiEdit
+                className={`edit-icon ${item.completed ?'completed' : 'unComplete'}`}
                 size="18px"
                 color="white"
           onClick={edit}
           cursor="pointer"
-        />
+        />}
               <RiDeleteBin5Fill
-                className="delete-icon icon"
+                className={`${item.completed ? 'completed delete-icon':'delete-icon' }`}
                 size="18px"
                 cursor="pointer"
-                color="white"
+                color="red"
                 onClick={deleteItem}
               />
           </div>
           <div className="todo-body">
           <div className="todo-status-container">
               <BsCheck2Circle
-              className="check-icon icon"
+              className={`${item.completed ? 'completed check-icon':'check-icon' }`}
               size="40px"
-            color="white"
+            color={comp}
             onClick={completed}
             cursor="pointer"
               />
           </div>
-          <div className='todo-name'>{ item.name}</div>
+        <div className={`todo-name ${item.completed &&  'completed'}`}>{ item.name}</div>
           </div>
     </div>
   )
